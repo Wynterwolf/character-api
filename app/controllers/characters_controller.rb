914a51153b56d01d558ece0f20f02465c345156b
wynterwolf <shinyguns@gmail.com>
2021-01-03 01:42:55 -0600

    CRUD for char controller

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,2 +1,43 @@
 class CharactersController < ApplicationController
+
+    def index
+        @characters = Character.all
+        render json: @characters
+    end
+
+    def show
+        @characters = Character.find(params[:id])
+        render json: @characters
+    end
+
+    def create 
+        @characters = Character.create(
+            name: params[:name],
+            breed: params[:breed],
+            auspice: params[:auspice],
+            tribe: params[:tribe],
+            concept: params[:concept]
+        )
+        render json: @characters
+    end
+
+    def update
+        @characters = Character.find(params[:id])
+        @characters.update(
+            name: params[:name],
+            breed: params[:breed],
+            auspice: params[:auspice],
+            tribe: params[:tribe],
+            concept: params[:concept]
+        )
+        render json: @characters
+    end
+
+    def destroy
+        @characters = Character.all
+        @character = Character.find(params[:id])
+        @character.destroy
+        render json: @characters
+    end
+    
 end
