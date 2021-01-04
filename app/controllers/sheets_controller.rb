d4189be71dce9622e00d1dd14b0266f2fb423225
wynterwolf <shinyguns@gmail.com>
2021-01-04 14:54:41 -0600

    sheet/1 working with stats and character

--- app/controllers/sheets_controller.rb
+++ app/controllers/sheets_controller.rb
@@ -1,8 +1,13 @@
 class SheetsController < ApplicationController
 
     def index
-        sheets = Sheet.all
-        render json: sheets, include: [:character, :stat]
+        @sheets = Sheet.all
+        render json: @sheets, include: [:character, :stat]
+    end
+
+    def show
+        @sheets = Sheet.find(params[:id])
+        render json: @sheets, include: [:character, :stat]
     end
 
 end
--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -4,4 +4,24 @@ class StatsController < ApplicationController
         @stats = Stat.all
         render json: @stats
     end
+
+    def show
+        @stats = Stat.find(params[:id])
+        render json: @stats
+    end
+
+    def create
+        @stats = Stat.create(
+            strength: params[:strength],
+            charisma: params[:charisma],
+            perception: params[:perception],
+            dexterity: params[:dexterity],
+            manipulation: params[:manipulation],
+            intelligence: params[:intelligence],
+            stamina: params[:stamina],
+            appearance: params[:appearance],
+            wit: params[:wit]
+        )
+        render json: @stats
+    end
 end
