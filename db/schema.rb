3c41ff6bbf0f2246e45a6fb90192e19676d30de3
Wynterwolf <shinyguns@gmail.com>
2021-01-07 10:42:41 -0600

    added user resource, associations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_03_060928) do
+ActiveRecord::Schema.define(version: 2021_01_07_162224) do
 
   create_table "characters", force: :cascade do |t|
     t.string "name"
@@ -27,6 +27,14 @@ ActiveRecord::Schema.define(version: 2021_01_03_060928) do
     t.integer "stamina"
     t.integer "appearance"
     t.integer "wit"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "username"
+    t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,5 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 char_a = Character.create(name: 'This Name', breed: 'This Breed', auspice: 'This Auspice', tribe: 'The Tribe', concept: 'Concept Here', strength: 1, charisma: 1, perception: 1, dexterity: 1, manipulation: 1, intelligence: 1, stamina: 1, appearance: 1, wit: 1)
+
+Jenny = User.create(name: "Jenny from the block", username: "J-lowlow", email: "thatgirl@thatgirl.com")
