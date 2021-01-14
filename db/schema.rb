f556a0a41eb5a307f112e2215eb01564863c078a
Wynterwolf <shinyguns@gmail.com>
2021-01-14 13:40:42 -0600

    added user to DB with migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_07_162224) do
+ActiveRecord::Schema.define(version: 2021_01_14_193031) do
 
   create_table "characters", force: :cascade do |t|
     t.string "name"
@@ -29,6 +29,8 @@ ActiveRecord::Schema.define(version: 2021_01_07_162224) do
     t.integer "wit"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_characters_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -39,4 +41,5 @@ ActiveRecord::Schema.define(version: 2021_01_07_162224) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "characters", "users"
 end
